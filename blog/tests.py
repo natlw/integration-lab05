c7ddc73ce903760293bc1ba210cb450afd3db1d6
natlw <natlewa021@gmail.com>
2026-06-04 16:31:35 +0200

    Add unit tests for the application

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+
+class BlogLogicTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="tester", password="test123")
+
+    def test_post_string_representation_returns_title(self):
+        """Test modelu: sprawdza, czy reprezentacja stringowa posta zwraca tytuł."""
+        post = Post.objects.create(
+            title="Moj testowy post", content="Treść posta", author=self.user
+        )
+        self.assertEqual(str(post), "Moj testowy post")
+
+    def test_post_list_view_contains_created_post(self):
+        """Test widoku: sprawdza, czy strona listy postów zawiera utworzony wpis."""
+        post = Post.objects.create(
+            title="Widoczny post", content="Treść", author=self.user
+        )
+        response = self.client.get(reverse("post-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(post, response.context["posts"])
